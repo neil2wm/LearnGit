db18882a15e34c431c5cfb94b3fd03a934e21bc5
neil <neil2wm@gmail.com>
2015-11-19 14:10:09 +0800

    modified a.cpp

--- a.cpp
+++ a.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Car {
+private:
+    int number;
+    int weight;
+    string name;
+public:
+    Car();
+    void run();
+};
+
+Car::Car() {
+     number = 0;
+     weight = 0;
+     name = "Benz";
+}
+
+void Car::run() {
+     cout << name << "runs" << endl;
+}
+
 int main() {
+    Car car;
+    car.run();
     cout << "Hello World!" << endl;
 }
